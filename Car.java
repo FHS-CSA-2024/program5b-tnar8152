0a6ee49d231b9bb5007967bce31c398852c7b08b
tnar8152 <77207056+tnar8152@users.noreply.github.com>
2024-10-21 08:07:47 -0700

    Car.java

--- Car.java
+++ Car.java
@@ -1,1 +1,55 @@
+public class Car
+{
+    //carName, milesDriven, and gallonsUsed
+    private String carName;
+    private double milesDriven;
+    private double gallonsUsed;
+    
+    public Car() {
+        carName = "";
+        milesDriven = 0;
+        gallonsUsed = 0;
+    }
+
+
+    public Car(String carName, double milesDriven,double gallonsUsed )
+    {
+        carName = carName;
+        milesDriven = milesDriven;
+        gallonsUsed = gallonsUsed;
+        
+    }
+    public void setCarName(String carName){
+        this.carName = carName;
+    }
+    public String getCarName(){
+        return carName;
+    }
+    public void setMilesDriven(double milesDriven){
+        this.milesDriven = milesDriven;
+    }
+    public double getMilesDriven() {
+        return milesDriven;
+    }
+    public void setGallonsUsed(double gallonsUsed){
+        this.gallonsUsed = gallonsUsed;
+    }
+    public double getGallonsUsed() {
+        return gallonsUsed;
+    }
+    public double calculateAverage(){
+        double milesDriven = getMilesDriven();
+        double gallonsUsed = getGallonsUsed();
+        
+        double averageMiles= Math.round((milesDriven / gallonsUsed) * 10.0)/10.0;
+        return averageMiles;
+    }
+    public String toString() {
+       return " Car Nmae " + carName +
+              ", Miles Driven " + milesDriven +
+              ", Gallons Used " + gallonsUsed;       
+       
+    }
+}
+
 
